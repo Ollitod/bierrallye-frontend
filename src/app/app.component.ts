fe01abcc66f2923c66196750743572612ec3dbb0
Tod Oliver <olivertod11@yahoo.de>
2023-05-30 19:19:56 +0200

    Migrate to standalone (standalone bootstrapping API)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FooterComponent} from './footer/footer.component';
+import {DeregisterComponent} from './deregister/deregister.component';
+import {RegisterComponent} from './register/register.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {InfoComponent} from './info/info.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    InfoComponent,
+    MatTabsModule,
+    RegisterComponent,
+    DeregisterComponent,
+    FooterComponent
+  ]
 })
 export class AppComponent {
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app/app-routing.module';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, MatTabsModule),
+    provideAnimations()
+  ]
+})
   .catch(err => console.error(err));
