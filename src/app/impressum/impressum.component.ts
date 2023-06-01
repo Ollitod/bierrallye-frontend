b8d8030fbb501c666e3306e253b2d081b08c3e7a
Tod Oliver <olivertod11@yahoo.de>
2023-06-01 17:43:21 +0200

    Make app responsive and add impressum

---
+++ src/app/impressum/impressum.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-impressum',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './impressum.component.html',
+  styleUrls: ['./impressum.component.scss']
+})
+export class ImpressumComponent {
+
+}
