c204581f45a78c7fb10faa00617aea28c7ba484a
Tod Oliver <olivertod11@yahoo.de>
2023-06-12 19:55:09 +0200

    Implement draft for race component

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WebcamModule} from 'ngx-webcam';
 import {MatButtonModule} from '@angular/material/button';
+import {ITeam} from '../shared/model/team.model';
 
 @Component({
   selector: 'app-race',
@@ -12,11 +13,26 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class RaceComponent {
 
-  open = false;
+  scannerOpenCheckin = false;
+  scannerOpenCheckout = false;
 
-  setOpen(): void {
-    this.open = true;
-    console.log('camera opened');
+  team: ITeam = {
+    startblock: 'Startblock A: 13:30 Uhr',
+    teamFirstMember: 'Oliver Tod',
+    teamSecondMember: 'Johannes Meinhard',
+    // startTime: new Date(2023, 6, 12, 17, 35, 48),
+    // endTime: new Date(2023, 6, 12, 19, 48, 25),
+    uuid: 'random',
+    email: 'olivertod11@yahoo.de',
+    boxId: 1
+  }
+
+  openScannerCheckin(): void {
+    this.scannerOpenCheckin = !this.scannerOpenCheckin;
+  }
+
+  openScannerCheckout(): void {
+    this.scannerOpenCheckout = !this.scannerOpenCheckout;
   }
 
   facingMode: string = 'environment'; // Set rear camera
