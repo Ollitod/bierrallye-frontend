a7455a24c511560338bc325ebb50c25b9320c432
Tod Oliver <olivertod11@yahoo.de>
2023-06-14 01:52:15 +0200

    Dummy: Display user info in header (replaces banner)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,21 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {IUser} from '../shared/model/user.model';
+import {Role} from '../shared/model/role';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    standalone: true,
+    imports: [CommonModule, RouterLink, MatButtonModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+    user: IUser = {
+        username: 'Admin',
+        role: Role.ADMIN
+    }
 }
